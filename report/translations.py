9e83d23b4d41508060c416148776bf72e6fdc5e6
Gedev <gerald.devreux.pro@gmail.com>
2024-08-03 15:15:22 +0800

    [IMP] Translations : 'fr', 'en' supported languages

---
+++ report/translations.py
@@ -0,0 +1,10 @@
+TRANSLATIONS = {
+    "Less Trash Damage": "Moins de dégâts sur les mobs",
+    "Max Healthstones": "Utilisation maximale des pierres de soin",
+    "Max Health stones used": "Pierres de soin utilisées au maximum",
+    "Max Potions": "Potions utilisées au maximum",
+    "Pull Before Tanks": "Attaque avant les tanks",
+    "Player pulled before tank the most": "Le joueur a attaqué avant le tank le plus souvent",
+    "Max Deaths": "Morts maximales",
+    "Player died the most times": "Le joueur est mort le plus de fois"
+}
--- report/views.py
+++ report/views.py
@@ -1,24 +1,43 @@
-from django.shortcuts import render
 from django.http import JsonResponse
-from django.views.decorators.csrf import csrf_exempt
-from django.views.decorators.http import require_POST
 from api.models import HighlightDetails
 import json
 
-# Create your views here.
+from report.translations import TRANSLATIONS
+
+
+def detect_language(request):
+
+    accept_language = request.headers.get('Accept-Language', '')
+    languages = [lang.split(';')[0] for lang in accept_language.split(',')]
+
+    supported_languages = ['fr', 'en']
+
+    for lang in languages:
+        if lang[:2] in supported_languages:
+            return lang[:2]
+
+    return 'en'
+
+
+def translate(highlights, language="fr"):
+    if language == "fr":
+        for item in highlights:
+            item['title'] = TRANSLATIONS.get(item['title'], item['title'])
+            item['description'] = TRANSLATIONS.get(item['description'], item['description'])
+    return highlights
 
 
 def get_highlights(request, **kwargs):
     try:
         report_code = kwargs.get('reportcode')
-        print(report_code)
         if report_code:
             highlights = HighlightDetails.objects.filter(report_id=report_code).values()
-            return JsonResponse(list(highlights), safe=False)
+            highlights_list = list(highlights)
+
+            language = detect_language(request)
+            translated_highlights = translate(highlights_list, language=language)
+            return JsonResponse(translated_highlights, safe=False)
         else:
             return JsonResponse({'error': 'Report code not provided'}, status=400)
     except json.JSONDecodeError:
         return JsonResponse({'error': 'Invalid JSON'}, status=400)
-
-
-
